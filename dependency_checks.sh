7f5753f096c8b1fe4332e92933c5420f40e0883e
MaxBrandtner <110912574+MaxBrandtner@users.noreply.github.com>
2023-02-05 19:17:56 +0700

    add dependency checks #8

---
+++ dependency_checks.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+awk --help >/dev/null || ( echo "missing dependency: awk" >/dev/stderr && exit 1 )
+sed --help >/dev/null || ( echo "missing dependency: sed" >/dev/stderr && exit 1 )
+jq --help  >/dev/null || ( echo "missing dependency: jq"  >/dev/stderr && exit 1 )
--- main.sh
+++ main.sh
@@ -2,7 +2,9 @@
 source functions/lib_json.sh || return 1
 cd "$(dirname "$(realpath "$BASH_SOURCE")")"
 
-json_input_data=$(obtain_json_data "$1" || echo "{}" ) 
+bash dependency_checks.sh || exit 1
+
+json_input_data=$(obtain_json_data "$1" || echo "{}" )
 
 json_input_data=$(bash complete_json_data.sh $json_input_data) || return 1
 
