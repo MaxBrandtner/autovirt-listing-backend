f3926af998980d749f889239ac8b276b4fa9c750
MaxBrandtner <110912574+MaxBrandtner@users.noreply.github.com>
2023-02-05 19:22:30 +0700

    add xrandr to dependency checks

--- dependency_checks.sh
+++ dependency_checks.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-awk --help >/dev/null || ( echo "missing dependency: awk" >/dev/stderr && exit 1 )
-sed --help >/dev/null || ( echo "missing dependency: sed" >/dev/stderr && exit 1 )
-jq --help  >/dev/null || ( echo "missing dependency: jq"  >/dev/stderr && exit 1 )
+awk --help    >/dev/null || ( echo "missing dependency: awk"     >/dev/stderr && exit 1 )
+sed --help    >/dev/null || ( echo "missing dependency: sed"     >/dev/stderr && exit 1 )
+jq --help     >/dev/null || ( echo "missing dependency: jq"      >/dev/stderr && exit 1 )
+xrandr --help >/dev/null || ( echo "missing dependency: xrandr"  >/dev/stderr && exit 1 )
