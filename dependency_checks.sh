e1663fa98057910c16fd5756ec7658c1d7962b3a
MaxBrandtner <110912574+MaxBrandtner@users.noreply.github.com>
2023-02-06 14:56:26 +0700

    add git dependency check

--- dependency_checks.sh
+++ dependency_checks.sh
@@ -4,3 +4,4 @@ awk --help    >/dev/null || ( echo "missing dependency: awk"     >/dev/stderr &&
 sed --help    >/dev/null || ( echo "missing dependency: sed"     >/dev/stderr && exit 1 )
 jq --help     >/dev/null || ( echo "missing dependency: jq"      >/dev/stderr && exit 1 )
 xrandr --help >/dev/null || ( echo "missing dependency: xrandr"  >/dev/stderr && exit 1 )
+git --help    >/dev/null || ( echo "missing dependency: xrandr"  >/dev/stderr && exit 1 )
